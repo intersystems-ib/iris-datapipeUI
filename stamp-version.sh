72cfd06043e088e60714b08a96006c118522f4e6
Alberto Fuentes <afuentes@ES-MBP-AFUENTES.local>
2020-06-05 10:03:44 +0200

    stamp-version, change sed separator to allow slash

--- stamp-version.sh
+++ stamp-version.sh
@@ -4,5 +4,5 @@
 SRCVERSION=`git log --pretty=format:'%h' -1`
 SRCTOOLTIP=`git log --pretty=format:'%h %ad %s' -1 --date=iso`
 echo ">>> stamping $SRCTOOLTIP"
-sed -i "s/@srcVersion/$SRCVERSION/" src/app/shared/info.service.ts
-sed -i "s/@srcTooltip/$SRCTOOLTIP/" src/app/shared/info.service.ts
+sed -i "s~@srcVersion~$SRCVERSION~" src/app/shared/info.service.ts
+sed -i "s~@srcTooltip~$SRCTOOLTIP~" src/app/shared/info.service.ts
