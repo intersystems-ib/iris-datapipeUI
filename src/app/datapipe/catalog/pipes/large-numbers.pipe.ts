b00ca4f18e9884871d18dd3713f917e3cd2e43da
omunozme <omunozme@intersystems.com>
2025-12-16 15:11:21 +0100

    patched issue with pipe with difference in null and undefined

--- src/app/datapipe/catalog/pipes/large-numbers.pipe.ts
+++ src/app/datapipe/catalog/pipes/large-numbers.pipe.ts
@@ -7,7 +7,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LargeNumbersPipe implements PipeTransform {
 
   transform(num: number): string {
-    if (num === null) return ''
+    if (num === undefined ) return ''
+    if (num === null ) return ''
     if (num >= 1000000) {
       return (num / 1000000).toFixed(1) + 'M';
     } else if (num >= 1000) {
